998715b32a555bdec06b6efad0b851fb7426c816
JustGentle <justgentle@sina.com>
2019-06-30 12:34:54 +0800

    #右键菜单尝试

---
+++ Just.WPF/Converters/ValueToVisibilityConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Just.WPF.Converters
+{
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class ValueToVisibilityConverter : IValueConverter
+    {
+        public ValueToVisibilityConverter()
+            : this(true)
+        {
+
+        }
+        public ValueToVisibilityConverter(bool collapsewhenInvisible)
+            : base()
+        {
+            CollapseWhenInvisible = collapsewhenInvisible;
+        }
+        public bool CollapseWhenInvisible { get; set; }
+
+        public Visibility FalseVisible
+        {
+            get
+            {
+                if (CollapseWhenInvisible)
+                {
+                    return Visibility.Collapsed;
+                }
+                else
+                {
+                    return Visibility.Hidden;
+                }
+            }
+
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return FalseVisible;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
