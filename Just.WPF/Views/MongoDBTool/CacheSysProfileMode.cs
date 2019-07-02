871052ba3af2ab627be568f648da496cb85e7abf
JustGentle <justgentle@sina.com>
2019-07-02 11:07:17 +0800

    #忽略严格结构匹配

--- Just.WPF/Views/MongoDBTool/CacheSysProfileMode.cs
+++ Just.WPF/Views/MongoDBTool/CacheSysProfileMode.cs
@@ -1,8 +1,10 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
 
 namespace Just.WPF.Views.MongoDBTool
 {
+    [BsonIgnoreExtraElements]
     public class CacheSysProfileMode
     {
         public ObjectId Id { get; set; }
