a1a7350730195fb4f234eb5ac0e13f474663cf9b
JustGentle <justgentle@sina.com>
2020-03-05 19:18:53 +0800

    可停止执行SQL

--- Just.Data/SqlCtrl.xaml.cs
+++ Just.Data/SqlCtrl.xaml.cs
@@ -8,6 +8,7 @@ using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -51,55 +52,93 @@ namespace Just.Data
             HighlightingManager.Instance.RegisterHighlighting("Custom", new string[] { ".sql" }, customHighlighting);
         }
 
+        private CancellationTokenSource cts;
+        private CancellationToken token;
         private void ButtonExec_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 tab.Items.Clear();
-                using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(cnn.Text))
+                cts = new CancellationTokenSource();
+                token = cts.Token;
+                ButtonExec.Visibility = Visibility.Collapsed;
+                ButtonStop.Visibility = Visibility.Visible;
+                var connstr = cnn.Text;
+                var sql = codeEditor.Document.Text;
+                Task.Run(() =>
                 {
-                    using (var reader = connection.ExecuteReader(codeEditor.Document.Text))
+                    using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(connstr))
                     {
-                        while (!reader.IsClosed && reader.FieldCount > 0)
+                        //显示时间
+                        var start = DateTime.Now;
+                        var task = Task.Run(() =>
                         {
-                            var table = new DataTable();
-                            table.Load(reader);
-                            tab.Items.Add(new TabItem
+                            while (!token.IsCancellationRequested && ButtonStop.Visibility == Visibility.Visible)
                             {
-                                Header = $"{table.Rows.Count}行",
-                                Content = new DataGrid
-                                {
-                                    AutoGenerateColumns = true,
-                                    IsReadOnly = true,
-                                    ItemsSource = table.AsDataView()
-                                },
-                                IsSelected = tab.Items.Count == 0
-                            });
-                        }
-                        if (reader.RecordsAffected != -1)
+                                MainWindowVM.ShowStatus($"{DateTime.Now - start}");
+                                Thread.Sleep(1000);
+                            }
+                        }, token);
+
+                        using (var reader = connection.ExecuteReaderAsync(new CommandDefinition(sql, cancellationToken: token)).Result)
                         {
-                            var table = new DataTable();
-                            table.Columns.Add(nameof(reader.RecordsAffected));
-                            table.Rows.Add(reader.RecordsAffected);
-                            tab.Items.Add(new TabItem
+                            MainWindowVM.ShowStatus($"{DateTime.Now - start}");
+                            MainWindowVM.DispatcherInvoke(() =>
                             {
-                                Header = $"{reader.RecordsAffected}行",
-                                Content = new DataGrid
+                                if (token.IsCancellationRequested) return;
+                                while (!reader.IsClosed && reader.FieldCount > 0)
                                 {
-                                    AutoGenerateColumns = true,
-                                    IsReadOnly = true,
-                                    ItemsSource = table.AsDataView()
-                                },
-                                IsSelected = tab.Items.Count == 0
+                                    var table = new DataTable();
+                                    table.Load(reader);
+                                    tab.Items.Add(new TabItem
+                                    {
+                                        Header = $"{table.Rows.Count}行",
+                                        Content = new DataGrid
+                                        {
+                                            AutoGenerateColumns = true,
+                                            IsReadOnly = true,
+                                            ItemsSource = table.AsDataView()
+                                        },
+                                        IsSelected = tab.Items.Count == 0
+                                    });
+                                }
+                                if (reader.RecordsAffected != -1)
+                                {
+                                    var table = new DataTable();
+                                    table.Columns.Add(nameof(reader.RecordsAffected));
+                                    table.Rows.Add(reader.RecordsAffected);
+                                    tab.Items.Add(new TabItem
+                                    {
+                                        Header = $"{reader.RecordsAffected}行",
+                                        Content = new DataGrid
+                                        {
+                                            AutoGenerateColumns = true,
+                                            IsReadOnly = true,
+                                            ItemsSource = table.AsDataView()
+                                        },
+                                        IsSelected = tab.Items.Count == 0
+                                    });
+                                }
+                                ButtonExec.Visibility = Visibility.Visible;
+                                ButtonStop.Visibility = Visibility.Collapsed;
                             });
                         }
                     }
-                }
+                }, token);
             }
             catch (Exception ex)
             {
+                ButtonExec.Visibility = Visibility.Visible;
+                ButtonStop.Visibility = Visibility.Collapsed;
                 MainWindowVM.NotifyError(ex.Message);
             }
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            cts?.Cancel();
+            ButtonExec.Visibility = Visibility.Visible;
+            ButtonStop.Visibility = Visibility.Collapsed;
         }
     }
 }
